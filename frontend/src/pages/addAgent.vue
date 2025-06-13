e061fc220b28cc433e5a99c2a117d1362b97ae05
merikhimo <merikhimo@gmail.com>
2025-06-13 17:58:36 +0300

    поле для кастомного создания агента

--- frontend/src/pages/addAgent.vue
+++ frontend/src/pages/addAgent.vue
@@ -2,10 +2,7 @@
   <v-form v-model="valid">
     <v-container>
       <v-row>
-        <v-col
-          cols="12"
-          md="4"
-        >
+        <v-col>
           <v-text-field
             v-model="name"
             label="First name"
@@ -13,10 +10,7 @@
           ></v-text-field>
         </v-col>
 
-        <v-col
-          cols="12"
-          md="4"
-        >
+        <v-col>
           <v-select
         v-model="OS"
         :items="OSs"
@@ -24,7 +18,60 @@
         required/>
         </v-col>
 
+        <v-col>
+          <v-select
+        v-model="modelTemplate"
+        :items="modelTemplates"
+        label="Role"
+        required/>
+        </v-col>
+
       </v-row>
+        <v-container v-if="modelTemplate === 'Create role'">
+            <v-row>
+                <v-col class="overflow-y-auto" style="max-height: 400px;">
+                    <v-text-field v-for="app in apps">
+                        <template #append-inner>
+                            <v-icon @click="clearText" class="cursor-pointer" color="grey">
+                                mdi-close
+                            </v-icon>
+                        </template>
+                    </v-text-field>
+                    <v-btn @click="addApp">Add App</v-btn>
+                    
+                </v-col>
+                <v-col class="overflow-y-auto" style="max-height: 400px;">
+                    <v-text-field v-for="page in pages">
+                        <template #append-inner>
+                            <v-icon @click="deleteApp" class="cursor-pointer" color="grey">
+                                mdi-close
+                            </v-icon>
+                        </template>
+                    </v-text-field>
+                    <v-btn @click="addPage">Add page</v-btn>
+                </v-col>
+                <v-col class="overflow-y-auto" style="max-height: 400px;">
+                    <v-container v-for="brake in brakes">
+                        <v-row>
+                            <v-col>
+                                <v-text-field label="start">
+                                </v-text-field>
+                            </v-col>
+                            <v-col>
+                                <v-text-field label="duration">
+                                </v-text-field>
+                            </v-col>
+                        </v-row>                    <template #append-inner>
+                            <v-icon @click="clearText" class="cursor-pointer" color="grey">
+                                mdi-close
+                            </v-icon>
+                        </template>
+                    </v-container>
+                    <v-btn @click="addbrake">Add brake</v-btn>
+                </v-col>
+            </v-row>  
+        </v-container>
+        <v-btn @click="submit">submit</v-btn>
     </v-container>
   </v-form>
 </template>
@@ -32,7 +79,32 @@
 const OSs = [
     'WINDOWS',
     'LINUX'  ]
+const modelTemplates = [
+    'Engineer',
+    'Financist',
+    'Developer',
+    'Create role'
+]
     const  name= ref('');
-    const OS=ref('')
+    const OS=ref('');
+    const modelTemplate=ref('');
+    const apps=ref([])
+    const pages=ref([])
+    const brakes=ref([])
+
+    function addApp() {
+        const newApp = '';
+        apps.value.push(newApp);
+    }
+
+    function addPage() {
+        const newPage = '';
+        pages.value.push(newPage);
+    }
+
+    function addbrake() {
+        const newBrake = '';
+        brakes.value.push(newBrake);
+    }
 
 </script>
